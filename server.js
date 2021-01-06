0ce7ed8e01d962a6c5b7546c0b98141616516924
Agrit Tiwari <agrit1408@gmail.com>
2021-01-06 11:23:57 +0530

    defining Routes

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.json({ msg : "started building backend"}))
 
+
+//Defining Routes 
+
+app.use('/myPortfolio/auth', require('./Routes/auth'));
+app.use('/myPortfolio/user', require('./Routes/user'));
+app.use('/myPortfolio/portfolio', require('./Routes/portfolio'));
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
