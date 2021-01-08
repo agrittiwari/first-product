0378444f156f789888893db5fd8734a42191e2d9
Agrit Tiwari <agrit1408@gmail.com>
2021-01-08 18:38:18 +0530

    using model and doing some error checking by sending -req.body - in response

--- Routes/register.js
+++ Routes/register.js
@@ -3,12 +3,19 @@
 const express = require('express')
 const router = express.Router()
 
+
+const Register = require('../models/Register')
+
+
 // @route POST  /myPortfolio/register
 //@details  to register a user on app
 //access     PRIVATE
 
 
-router.post('/', (req, res) => res.send('Registering a user'))
+router.post('/', (req, res) =>
+{
+    res.send('Registering a user')
+})
 
 
 module.exports = router;
