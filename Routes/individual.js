f482fdfed9d98e2db75d669f80128510f1a9039e
Agrit Tiwari <agrit1408@gmail.com>
2021-01-20 12:47:05 +0530

    Building get request for the profile access

--- Routes/individual.js
+++ Routes/individual.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('auth')
+
 const Individual= require('../models/Individual')
+const Register = require('../models/Register')
 
-
-// @route GET  /myPortfolio/{username}
+// @route GET  /me
 //@details  get individual portfolio
-//access     PUBLIC
-router.get('/{id}/username', (req, res) =>
+//access     PRIVATE
+router.get('/me', auth, async(req, res) =>
 {
-    res.send('get individual portfolios')
+    try {
+        
+        const individualProfile = await Individual.findOne({ register: req.register.id }).populate('register', ['name'])
+        
+        if (!individualProfile) {
+            res.status(400).json({msg:"This individual has no profile"})
+        }
+        
+        res.send('Profile found').json(individualProfile);
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({msg: 'Server Error'})
+    }
 })
 
 module.exports = router;
