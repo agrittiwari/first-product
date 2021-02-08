8b7120b909cb0a7114d8fe6b2f20afde19b57e92
Agrit Tiwari <agrit1408@gmail.com>
2021-02-08 20:29:06 +0530

    Making forms interactive

--- my-portfolio/src/Components/Auth/Register.js
+++ my-portfolio/src/Components/Auth/Register.js
@@ -1,29 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../../App.css"
 
 import {Link } from 'react-router-dom'
 
 
-const Register = () => {
+const Register = () =>
+{
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2:''
+  })
+
+  const {name, email, password, password2} = formData
+ 
+  const onSubmit= e => 
+  {
+    if (password !== password2) {
+      alert('password do not match')
+    }
+    setFormData({ ...formData, [e.target.name]:[e.target.value]});
+ }
     return (
         <div>
-             <nav className="navbar bg-dark">
-     
-      <ul>
+    <nav className="navbar bg-dark">
+     <ul>
         <li><Link to="/" >HomePage</Link></li>
-        
         <li><Link to="/login" >Login</Link></li>
       </ul>
     </nav>
     <section className="container">
       <h1 className="large text-primary">Sign Up</h1>
       <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-      <form className="form" action="create-profile.html">
+      <form className="form" >
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" required />
+              <input type="text" placeholder="Name" name="name"
+                value= {name}    onChange={onSubmit}  required />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" />
+              <input type="email" placeholder="Email Address"
+          value={email} onChange={onSubmit}     name="email" />
           
         </div>
         <div className="form-group">
@@ -32,6 +49,8 @@ const Register = () => {
             placeholder="Password"
             name="password"
             minLength="6"
+            value={password}
+            onChange={onSubmit}    
           />
         </div>
         <div className="form-group">
@@ -40,9 +59,11 @@ const Register = () => {
             placeholder="Confirm Password"
             name="password2"
             minLength="6"
+            value={password2}
+            onChange={onSubmit}    
           />
         </div>
-        <input type="submit" className="btn btn-primary" value="Register" />
+        <input type="submit" className="btn btn-primary" onSubmit={onSubmit} value="Register" />
       </form>
       <p className="my-1">
         Already have an account? <Link to="/login" >Sign In</Link>
